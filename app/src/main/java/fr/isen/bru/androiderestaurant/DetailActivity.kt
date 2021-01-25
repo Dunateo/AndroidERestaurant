143d1a096710e61ca754505be53fad82605cc238
Valentin <perso@valentinbru.com>
2021-01-25 11:55:05 +0100

    ajout d'un autre truc sympa

---
+++ app/src/main/java/fr/isen/bru/androiderestaurant/DetailActivity.kt
@@ -0,0 +1,39 @@
+package fr.isen.bru.androiderestaurant
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailActivity : AppCompatActivity() {
+    var foodDescription: TextView? = null
+    var RecipeName: TextView? = null
+    var RecipePrice: TextView? = null
+    var foodImage: ImageView? = null
+    var key: String? = ""
+    var imageUrl: String? = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+        RecipeName = findViewById<View>(R.id.txtRecipeName) as TextView
+        RecipePrice = findViewById<View>(R.id.txtPrice) as TextView
+        foodDescription = findViewById<View>(R.id.txtDescription) as TextView
+        foodImage = findViewById<View>(R.id.ivImage2) as ImageView
+        val mBundle = intent.extras
+        if (mBundle != null) {
+            foodDescription!!.text = mBundle.getString("Description")
+            key = mBundle.getString("keyValue")
+            imageUrl = mBundle.getString("Image")
+            RecipeName!!.text = mBundle.getString("RecipeName")
+            RecipePrice!!.text = mBundle.getString("price")
+            // foodImage.setImageResource(mBundle.getInt("Image"));
+
+        }
+    }
+
+
+}
---
+++ app/src/main/java/fr/isen/bru/androiderestaurant/HomeActivity.kt
@@ -0,0 +1,41 @@
+package fr.isen.bru.androiderestaurant
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+import kotlin.collections.ArrayList
+
+class HomeActivity : AppCompatActivity() {
+
+    var myFoodList: List<FoodData>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        myFoodList = ArrayList()
+        mano(myFoodList)
+    }
+
+    fun mano(arrayList: ArrayList<FoodData>){
+        val elem = FoodData()
+        arrayList.add(elem);
+        arrayList.add(elem);
+        arrayList.add(elem);
+    }
+    fun btnEntree(view: View?) {
+       val toat = Toast.makeText(this.applicationContext, "Entrées", Toast.LENGTH_SHORT)
+        toat.show()
+    }
+    fun btnRecipe(view: View?) {
+        startActivity(
+            Intent(applicationContext, DetailActivity::class.java)
+                .putExtra("recipeNameKey", myFoodList.s)
+
+        )
+    }
+
+
+}
